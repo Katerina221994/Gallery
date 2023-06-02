e4b904bec41106d53e0b50aadd04dfca2e55302f
Ekaterina <rfvb.foto@mail.ru>
2023-06-02 09:41:13 +0200

    v

---
+++ app/src/main/java/ru/bignerd/googlemap/MainActivity.kt
@@ -0,0 +1,25 @@
+package ru.bignerd.googlemap
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.bignerd.googlemap.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            val geoUriString = "google.streetview:cbll=59.939448,30.328264&cbp=1,99.56,,1,2.0&mz=19"
+            val geoUri:Uri = Uri.parse(geoUriString)
+            val mapIntent = Intent(Intent.ACTION_VIEW,geoUri)
+            if(mapIntent.resolveActivity(packageManager) != null){
+                startActivity(mapIntent)
+            }
+        }
+    }
+}
